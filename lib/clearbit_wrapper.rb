f74247d7578c184dd9bd9a4b6d20f388c088c25a
Charles Ellis <hamled@hamled.name>
2017-11-01 15:45:19 -0700

    Clearbit API wrapper for getting logo for domain
    The Clearbit API wrapper has been updated to have a method for accessing
    the Clearbit Logo API which takes a domain name and returns the image
    data for the logo in PNG format.
    
    Our wrapper then converts this PNG data into a valid data URL by
    encoding it as base64.

--- lib/clearbit_wrapper.rb
+++ lib/clearbit_wrapper.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'base64'
 
 class ClearbitWrapper
   COMPANY_BASE_URL = "https://company.clearbit.com/v1"
+  LOGO_BASE_URL = "https://logo.clearbit.com"
 
   AUTH_OPTIONS = {
     basic_auth: {
@@ -12,5 +14,16 @@ class ClearbitWrapper
   def self.domain_search(name)
     response = HTTParty.get("#{COMPANY_BASE_URL}/domains/find?name=#{name}", AUTH_OPTIONS)
     return response.parsed_response["domain"]
+  end
+
+  def self.logo_search(domain)
+    logo_png = HTTParty.get("#{LOGO_BASE_URL}/#{domain}").parsed_response
+    return nil unless logo_png.present?
+
+    # Convert the PNG data into an in-line PNG
+    prefix = "data:image/png;base64,"
+    logo_base64 = Base64.encode64(logo_png).chomp
+
+    return prefix + logo_base64
   end
 end
--- test/lib/clearbit_wrapper_test.rb
+++ test/lib/clearbit_wrapper_test.rb
@@ -14,4 +14,18 @@ describe ClearbitWrapper do
       end
     end
   end
+
+  describe "#logo_search" do
+    it "returns a data URL for domains with a known logo" do
+      VCR.use_cassette("logo_search") do
+        # Fill this in
+      end
+    end
+
+    it "returns nil for domains without a known logo" do
+      VCR.use_cassette("logo_search") do
+        # Fill this in
+      end
+    end
+  end
 end
