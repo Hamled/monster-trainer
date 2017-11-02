c95eece40dad845455d36629caf7a74f750fb983
Charles Ellis <hamled@hamled.name>
2017-11-01 17:00:30 -0700

    Implement tests for Clearbit API wrapper

--- test/lib/clearbit_wrapper_test.rb
+++ test/lib/clearbit_wrapper_test.rb
@@ -4,13 +4,14 @@ describe ClearbitWrapper do
   describe "#domain_search" do
     it "returns a domain name for known companies" do
       VCR.use_cassette("domain_search") do
-        # Fill this in
+        ClearbitWrapper.domain_search("Intel").must_equal "intel.com"
+        ClearbitWrapper.domain_search("Ada Developers Academy").must_equal "adadevelopersacademy.org"
       end
     end
 
     it "returns nil for unknown companies" do
       VCR.use_cassette("domain_search") do
-        # Fill this in
+        ClearbitWrapper.domain_search("Not A Real Company I Hope").must_be_nil
       end
     end
   end
@@ -18,13 +19,18 @@ describe ClearbitWrapper do
   describe "#logo_search" do
     it "returns a data URL for domains with a known logo" do
       VCR.use_cassette("logo_search") do
-        # Fill this in
+        logo_url = ClearbitWrapper.logo_search("intel.com")
+
+        # There are better tests we could do if we use the 'data_uri' gem
+        # Check it out here: https://github.com/dball/data_uri
+        logo_url.must_be_kind_of String
+        logo_url.must_match /^data:image\/png;base64/
       end
     end
 
     it "returns nil for domains without a known logo" do
       VCR.use_cassette("logo_search") do
-        # Fill this in
+        ClearbitWrapper.logo_search("thisshouldnotbearealwebsite.com").must_be_nil
       end
     end
   end
